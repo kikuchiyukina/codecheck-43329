9ee404be0ce20147cd152307fe563ab1289ce64e
tie303223 <tie303223@S1307C00780-T1.tisnt.tis.co.jp>
2018-06-21 22:48:19 +0900

    コミット

--- src/main/java/codecheck/App.java
+++ src/main/java/codecheck/App.java
@@ -1,10 +1,56 @@
 package codecheck;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.lang.StringBuilder;
+
 
 public class App {
 	public static void main(String[] args) {
-		for (int i = 0, l = args.length; i < l; i++) {
-			String output = String.format("argv[%s]: %s", i, args[i]);
-			System.out.println(output);
+
+		String val = request.getParameter(args);
+		String urlString = "http://challenge-server.code-check.io/api/hash?q=";
+
+		String url = urlString + val;
+
+		HttpURLConnection  urlConn = null;
+		InputStream in = null;
+		BufferedReader reader = null;
+
+		try {
+			URL url = new URL(url);
+			urlConn = (HttpURLConnection) url.openConnection();
+			urlConn.setRequestMethod("GET");
+			urlConn.connect();
+			int status = urlConn.getResponseCode();
+
+			if (status == HttpURLConnection.HTTP_OK) {
+				in = urlConn.getInputStream();
+				reader = new BufferedReader(new InputStreamReader(in));
+
+				StringBuilder output = new StringBuilder();
+				String line;
+				while ((line = reader.readLine()) != null) {
+					output.append(line);
+				}
+				System.out.println(output.toString());
+		      }
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (reader != null) {
+					reader.close();
+				}
+				if (urlConn != null) {
+					urlConn.disconnect();
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 		}
-	}
-}
+			}
+		}
